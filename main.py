d14527fd2741cc37c4e58f75d11363569af10e64
AndiCucka <124384763+AndiCucka@users.noreply.github.com>
2023-09-12 17:10:51 +0000

    New turing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Created by: Andi Cucka
+Created by: Andi Cucka2
 Created on: Sept 2023
 This is "Turing Machine" program that accepts the state machine rules
     and the initial "tape" and then runs
@@ -12,9 +12,8 @@ class Tape(object):
     
     blank_symbol = " "
     
-    def __init__(self,
-                 tape_string = ""):
-        self.__tape = dict((enumerate(tape_string)))
+    def __init__(self, tape_string=""):
+        self.__tape = dict(enumerate(tape_string))
         
     def __str__(self):
         s = ""
@@ -24,7 +23,7 @@ class Tape(object):
             s += self.__tape[i]
         return s    
    
-    def __getitem__(self,index):
+    def __getitem__(self, index):
         if index in self.__tape:
             print("Read:  " + self.__tape[index] + " at position: " + str(index))
             return self.__tape[index]
@@ -32,30 +31,31 @@ class Tape(object):
             return Tape.blank_symbol
 
     def __setitem__(self, pos, char):
-        print("Write: "+ char + " at position: " + str(pos))
+        print("Write: " + char + " at position: " + str(pos))
         self.__tape[pos] = char
-        print("Current tape: ", end = "")
+        print("Current tape: ", end="")
         for key, value in self.__tape.items():
-            print(value, end = "")
+            print(value, end="")
+        print()
 
         
 class TuringMachine(object):
     
     def __init__(self, 
-                 tape = "", 
-                 blank_symbol = " ",
-                 initial_state = "",
-                 final_states = None,
-                 transition_function = None):
+                 tape="",
+                 blank_symbol=" ",
+                 initial_state="",
+                 final_states=None,
+                 transition_function=None):
         self.__tape = Tape(tape)
         self.__head_position = 0
         self.__blank_symbol = blank_symbol
         self.__current_state = initial_state
-        if transition_function == None:
+        if transition_function is None:
             self.__transition_function = {}
         else:
             self.__transition_function = transition_function
-        if final_states == None:
+        if final_states is None:
             self.__final_states = set()
         else:
             self.__final_states = set(final_states)
@@ -76,10 +76,10 @@ class TuringMachine(object):
                 self.__head_position -= 1
                 print("  ↢ Move head 1 left")
             self.__current_state = y[0]
-            print("             ", end = "")
+            print("             ", end="")
             for head_position_counter in range(self.__head_position):
-                print(" ", end = "")
-            print("▲\n")
+                print(" ", end="")
+            print("▲")
             time.sleep(1.0)
 
     def final(self):
@@ -88,23 +88,18 @@ class TuringMachine(object):
         else:
             return False
 
-# state machine
-#                    ↓ current state
-#                         ↓ value read
-#                                 ↓ state you become
-#                                       ↓ value to write to Tape
-#                                            ↓ direction to move
-state_machine = {("init","0"):("init", "1", "R"),
-                 ("init","1"):("init", "0", "R"),
-                 ("init"," "):("final"," ", "N"),
-                }
+# State machine for counting from 11 to 16 in binary
+state_machine = {
+    ("init", "0"): ("init", "0", "R"),  # If seeing 0, keep it and move right
+    ("init", "1"): ("init", "1", "R"),  # If seeing 1, keep it and move right
+    ("init", " "): ("final", " ", "N"),  # If seeing blank, halt
+    ("init", "#"): ("add1", " ", "L"),  # If seeing # (end of tape), go to the first bit and move left
+    ("add1", "0"): ("init", "1", "N"),  # If seeing 0, change it to 1 and halt
+    ("add1", "1"): ("add1", "0", "L"),  # If seeing 1, change it to 0 and move left
+}
 
-# initial tape
-#                  ↓ initial tape values
-t = TuringMachine("010011001 ", 
-                  initial_state = "init",
-                  final_states = {"final"},
-                  transition_function = state_machine)
+# Initial tape (starting from binary 11)
+t = TuringMachine("11# ", initial_state="init", final_states={"final"}, transition_function=state_machine)
 
 print("Input on Tape:\n              " + t.get_tape())
 print("              ▲" + "\n")
